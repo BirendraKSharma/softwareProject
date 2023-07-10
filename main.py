b19b35e17ef5a85a677f7642f8def5b9c02f4c79
Birendra-dev <139046021+Birendra-dev@users.noreply.github.com>
2023-07-10 07:37:33 +0000

    register buttom modified

--- main.py
+++ main.py
@@ -30,5 +30,10 @@ def hello_world():
   return render_template('home.html', jobs=DOC)
 
 
+@app.route('/about')
+def show_about():
+  return render_template('about.html')
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
